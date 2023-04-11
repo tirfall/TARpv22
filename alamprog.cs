433b769166e0cadf36eb393582d6717ad376dcf5
opilane <opilane@HP-CZC2349HTB>
2023-04-11 11:27:46 +0300

    new ver

---
+++ alamprog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv22
+{
+    public class alamprog
+    {
+        public static string Kolmarv(int arv1)
+        {
+            string ans = null;
+            for (int x=0; x!=arv1; x++) 
+            {
+                ans = ans + "*";
+            }
+            return ans;
+        }
+        public static string Nurrcalc(int arv1, int arv2, char tehe) 
+        {
+            string var=null;
+            int vastus = 0;
+            switch (tehe)
+            {
+                case '+':
+                    vastus = arv1 + arv2;
+                    break;
+                case '-':
+                    vastus = arv1 - arv2;
+                    break;
+                case '*':
+                    vastus = arv1 * arv2;
+                    break;
+                case '/':
+                    if (arv2>arv1) { Console.WriteLine("vale numbrid"); }
+                    else{ vastus = arv1 / arv2; }
+                    
+                    break;
+            }
+            for (int x = 0; x!=vastus; x++)
+            {
+                var=var + "nurr ";
+            }
+            return var;
+        }
+        public static void Tarnid(int kuipal) 
+        { 
+            for (int x = 0; x!=kuipal; x++) { Console.Write("*"); }
+        }
+        public static int Korruta(int arv1, int arv2)
+        {
+            return arv1 * arv2;
+        }
+        public  static void Tere(string name)
+        {
+            Console.WriteLine("Tere {0}",name);
+        }
+    }
+}
--- start.cs
+++ start.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,8 +36,9 @@ namespace TARpv22
             //do { Console.WriteLine(paev); j--; } while (j!=0);
             //Console.WriteLine(j);
 
-            //int[] arvud=new int[10];
-            //for (int i = 0; i< arvud.Length; i++) { 
+            //int[] arvud = new int[10];
+            //for (int i = 0; i < arvud.Length; i++)
+            //{
             //    arvud[i] = rnd.Next(-100, 100);
             //}
             //foreach (int arv in arvud) { if (arv < 0) { Console.Beep(); } Console.WriteLine(arv); }
@@ -93,8 +95,7 @@ namespace TARpv22
             //Console.WriteLine("Arvude {0} ja {1}, põranda pindala võrdub {2}", pik1, pik2, pik1 * pik2);
             //Console.WriteLine("kas sa tahad teha remonti?");
             //string remans = Console.ReadLine();
-            //remans.ToLower();
-            //switch (remans)
+            //switch (remans.ToLower())
             //{
             //    case "jah":
             //        Console.WriteLine("Kui palju maksab ruutmeeter?");
@@ -128,24 +129,75 @@ namespace TARpv22
 
             //7 task
 
-            int ostud = 0;
-            for (int x = 0; x < 3; x++)
-            {
-                
-                switch
-            }
+            //Console.WriteLine("Mis on sinu pikkus?");
+            //int pikkus = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Mis on sinu segu? Naine või mees?");
+            //string segu = Console.ReadLine();
+            //if (segu.ToLower() == "mees" || segu.ToLower() == "naine")
+            //{
+            //    if (pikkus <= 170) { Console.WriteLine("Sa oled lühike ja sinu segu on {0}", segu); }
+            //    else if (pikkus <= 190) { Console.WriteLine("Sa oled keskmine ja sinu segu on {0}", segu); }
+            //    else if (pikkus >= 190) { Console.WriteLine("Sa oled pikk ja sinu segu on {0}", segu); }
+            //}
 
             //Kordused ülesanned
 
             //1 task
 
-            int[] masarvud = new int[10];
-            for (int k = 0; k < masarvud.Length; k++)
-            {
-                masarvud[k] = rnd.Next(-100, 100);
-                masarvud[k] = rnd.Next(-100, 100);
-            }
+            //int[] masarvud = new int[10];
+            //for (int k = 0; k < masarvud.Length; k++)
+            //{
+            //    masarvud[k] = rnd.Next(-100, 100);
+            //    masarvud[k] = rnd.Next(-100, 100);
+            //}
 
+            //2 task
+
+            //int[] array = new int[5];
+            //for (int h = 0; h != 5; h++) { array = int.Parse(Console.ReadLine()); }
+
+
+
+            //Alamprogrammid
+
+            //string nimi = "Aleks";
+            //alamprog.Tere(nimi);
+            //int a = 12;
+            //int b = 15;
+            //int vastus = alamprog.Korruta(a, b);
+            //Console.WriteLine(vastus);
+            //Console.WriteLine(alamprog.Korruta(5, 5));
+
+            //1 task
+
+
+
+            //2 task
+
+            //Console.WriteLine("Sisesta esimene arv:");
+            //int nurarv1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Sisesta teine arv:");
+            //int nurarv2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Sisesta tehe:");
+            //char nurtehe = char.Parse(Console.ReadLine());
+            //Console.Write(alamprog.Nurrcalc(nurarv1, nurarv2, nurtehe));
+
+            //3 task
+
+            //Console.WriteLine("Sisesta kui palju tärnid sa tahad:");
+            //int vastuskui = int.Parse(Console.ReadLine());
+            //alamprog.Tarnid(vastuskui);
+
+            //4 task
+            Console.WriteLine("Kui palju rida sa tahad?");
+            int vastus = int.Parse(Console.ReadLine());
+            for (int n = 0; n != vastus; n++)
+            {
+                Console.WriteLine("Sisesta number:");
+                int kolarv1 = int.Parse(Console.ReadLine());
+                Console.WriteLine(alamprog.Kolmarv(kolarv1));
+            }
+                
         }
     }
 }
